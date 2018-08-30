1a4aafd0017a8788e24acb8db028f6b1aea7304b
Dan Chung <danetix@gmail.com>
2018-08-29 20:10:23 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,16 @@
 // Code ControlledInput Component Here
+import React,  { Component } from 'react'
+
+export default class  extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+    <div></div>
+    )
+  }
+}
